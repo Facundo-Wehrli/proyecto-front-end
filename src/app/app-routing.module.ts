2c6eb73d6315bc7ac2c77ecb6b26bb5d33ef65ac
Facundo <wehrlifacundo9@gmail.com>
2023-10-06 09:08:03 -0300

    docs: any unknown route redirects to 'auth'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,13 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
+  /* Redirects any unknown route to the 'auth' route. This means that if
+  a user enters a URL that does not match any of the defined routes, they will be redirected to the
+  'auth' route. */
+  {
+    path: '**',
+    redirectTo: 'auth',
+  },
 ];
 
 @NgModule({

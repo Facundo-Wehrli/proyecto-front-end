3d0bf95a935989546f637bf45a9da5d24354c60c
Facundo <wehrlifacundo9@gmail.com>
2023-10-07 15:05:15 -0300

    feat: reactive Forms for register page

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../services/validators.service';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.css']
+  styleUrls: ['./register-page.component.css'],
 })
 export class RegisterPageComponent {
+  private fb = inject(FormBuilder);
+  private validatorsService = inject(ValidatorsService);
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            this.validatorsService.firstNameAndLastnamePattern
+          ),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorsService.emailPattern),
+        ],
+      ],
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2'),
+      ],
+    }
+  );
 
+  isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }

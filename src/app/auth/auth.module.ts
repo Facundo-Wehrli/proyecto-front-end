61924191ae3699b7d125a7e126875b4cb96cb184
Facundo <wehrlifacundo9@gmail.com>
2023-10-07 14:11:34 -0300

    update: import ReactiveFormsModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { RouterModule } from '@angular/router';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginPageComponent, RegisterPageComponent],
-  imports: [CommonModule, AuthRoutingModule, RouterModule],
+  imports: [CommonModule, AuthRoutingModule, RouterModule, ReactiveFormsModule],
 })
 export class AuthModule {}

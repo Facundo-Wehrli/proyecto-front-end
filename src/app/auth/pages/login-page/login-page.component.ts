f3679c127730f9554101f3ba8cf803ebe26118d0
Facundo <wehrlifacundo9@gmail.com>
2023-10-10 15:16:29 -0300

    feat: reactive form service implementation

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ValidatorsService } from '../../services/validators.service';
+import { FormServiceService } from '../../services/form-service.service';
 @Component({
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
-export class LoginPageComponent {
-
+export class LoginPageComponent implements OnInit {
+  public myForm: FormGroup = new FormGroup({});
+  ngOnInit(): void {
+    this.myForm = this.formService.createForm();
+  }
+  private validatorsService = inject(ValidatorsService);
+  private formService = inject(FormServiceService);
+  public isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
+  }
+  public onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }

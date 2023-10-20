2f4931769bb6911844f1baf36f78685fb08d71e8
Facundo <wehrlifacundo9@gmail.com>
2023-10-20 08:25:06 -0300

    chore: add hay-harvest module (pure arquitecture)

---
+++ src/app/hay-harvest/hay-harvest.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HayHarvestRoutingModule } from './hay-harvest-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HayHarvestRoutingModule
+  ]
+})
+export class HayHarvestModule { }

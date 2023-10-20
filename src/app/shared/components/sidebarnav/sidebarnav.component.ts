5281ff3b53d9ea9127438b02d5d9d16ce3e976a4
Facundo <wehrlifacundo9@gmail.com>
2023-10-20 08:25:54 -0300

    chore: add shared module (pure arquitecture)

---
+++ src/app/shared/components/sidebarnav/sidebarnav.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './sidebarnav.component.html',
+  styleUrls: ['./sidebarnav.component.css']
+})
+export class SidebarnavComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { faFontAwesome } from '@fortawesome/free-solid-svg-icons';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarnavComponent } from './components/sidebarnav/sidebarnav.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    SidebarnavComponent,
+    HeaderComponent
+  ],
   imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}

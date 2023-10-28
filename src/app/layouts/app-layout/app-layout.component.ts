df72a9bb34d34642e28bef322cefccde1ca826e9
Facundo <wehrlifacundo9@gmail.com>
2023-10-28 10:04:18 -0300

    delete unused/trash files

--- src/app/layouts/app-layout/app-layout.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './app-layout.component.html',
-  styleUrls: ['./app-layout.component.css']
-})
-export class AppLayoutComponent {
-
-}
--- src/app/shared/components/sidebarnav/sidebarnav.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './sidebarnav.component.html',
-  styleUrls: ['./sidebarnav.component.css']
-})
-export class SidebarnavComponent {
-
-}

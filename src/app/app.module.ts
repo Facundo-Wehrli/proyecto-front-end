2763bb4770f8a7935453369f37ed0c282fa58703
Facundo <wehrlifacundo9@gmail.com>
2023-10-28 09:57:34 -0300

    import: shared module & layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
-
+import { SharedModule } from './shared/shared.module';
+import { LayoutModule } from './layout/layout.module';
+import { LayoutComponent } from './layout/layout.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    AppLayoutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LayoutComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, LayoutModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

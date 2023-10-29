d218b4a0be4edf185c7b93ddf9e722965ca1f283
Facundo <wehrlifacundo9@gmail.com>
2023-10-28 21:35:32 -0300

    feat: implement shouldShowNavBar function on layout template, the function will display the navbar if the url isn't from the auth module

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent {
+  private router = inject(Router);
 
+/**
+ * The function shouldShowNavbar returns true if the current URL is not '/auth/login' or
+ * '/auth/register', and false otherwise.
+ */
+  shouldShowNavbar(): boolean {
+    const currentUrl = this.router.url;
+    return currentUrl !== '/auth/login' && currentUrl !== '/auth/register';
+  }
 }

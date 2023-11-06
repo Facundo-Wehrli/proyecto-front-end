cb6fb409ad15683a56741d319526291409fb5ae7
Facundo <wehrlifacundo9@gmail.com>
2023-11-06 10:52:21 -0300

    Add CoreModule to imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { LayoutModule } from './layout/layout.module';
 import { LayoutComponent } from './layout/layout.component';
+import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [AppComponent, LayoutComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, LayoutModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, LayoutModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })

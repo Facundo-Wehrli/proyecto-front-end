73538f596798c97e968c0b92e16aa62ca81766d2
Facundo <wehrlifacundo9@gmail.com>
2023-11-10 08:45:19 -0300

    Refactor AuthModule imports to use SharedModule.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,10 +5,10 @@ import { RouterModule } from '@angular/router';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LoginPageComponent, RegisterPageComponent],
-  imports: [CommonModule, AuthRoutingModule, RouterModule, ReactiveFormsModule],
+  imports: [CommonModule, AuthRoutingModule, RouterModule, SharedModule],
 })
 export class AuthModule {}
